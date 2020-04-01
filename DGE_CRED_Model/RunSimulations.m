b0af127390a28f175509846e30069fa69cba0b59
schultkr <schult.christoph@googlemail.com>
2020-04-01 14:06:52 +0200

    Add Drought Scenario

--- DGE_CRED_Model/RunSimulations.m
+++ DGE_CRED_Model/RunSimulations.m
@@ -5,6 +5,6 @@
 addpath('C:\dynare\4.6.1\matlab')
 % specify scenario names
 % casScenarioNames = {'Baseline', 'Temperature', 'SeaLevel', 'Adaptation'};
-casScenarioNames = {'Adaptation'};
+casScenarioNames = {'Drought'};
 % execute dynare to run the model
 dynare DGE_CRED_Model noclearall
