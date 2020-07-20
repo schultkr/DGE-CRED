9d3c7d92dde1ce42d8806d633cdaad113daad9c9
schultkr <schult.christoph@googlemail.com>
2020-07-20 15:25:21 +0200

    Update Folder
    remove commetns for slides
    remove literature folder

--- DGE_CRED_Model/+DGE_CRED_Model/static_g3.m
+++ DGE_CRED_Model/+DGE_CRED_Model/static_g3.m
@@ -18,6 +18,6 @@ function g3 = static_g3(T, y, x, params, T_flag)
 if T_flag
     T = DGE_CRED_Model.static_g3_tt(T, y, x, params);
 end
-g3 = sparse([],[],[],1668,345782336);
+g3 = sparse([],[],[],312,30371328);
 
 end
--- DGE_CRED_Model/+DGE_CRED_Model/static_g3_tt.m
+++ DGE_CRED_Model/+DGE_CRED_Model/static_g3_tt.m
@@ -13,7 +13,7 @@ function T = static_g3_tt(T, y, x, params)
 %   T         [#temp variables by 1]  double   vector of temporary terms
 %
 
-assert(length(T) >= 1781);
+assert(length(T) >= 319);
 
 T = DGE_CRED_Model.static_g2_tt(T, y, x, params);
 
