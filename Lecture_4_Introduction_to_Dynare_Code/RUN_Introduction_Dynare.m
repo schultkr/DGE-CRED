d332201d9cdb5008213667bccad83b7b75b78db7
schultkr <schult.christoph@googlemail.com>
2022-10-07 15:21:54 +0700

    Remove old material and add new report

--- Lecture_4_Introduction_to_Dynare_Code/RUN_Introduction_Dynare.m
+++
@@ -1,9 +0,0 @@
-clear all
-close all
-
-% This is the corresponding MATLAB file for the dynare file "solow.mod".
-
-disp '***************** NEW RUN ***********************'
-
-addpath /Applications/Dynare/4.6.1/matlab
-dynare Introduction_Dynare
